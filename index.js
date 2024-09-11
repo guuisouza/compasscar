ce1e51f3122486ae86b5199331b8f891822d913d
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-09-11 17:25:12 -0300

    feat: Implements PATCH method to update a car and its items

--- index.js
+++ index.js
@@ -123,4 +123,88 @@ app.delete('/api/v1/cars/:id', (req, res) => {
     })
 })
 
+app.patch('/api/v1/cars/:id', async (req, res) => {
+    const id = req.params.id
+
+    const brand = req.body.brand
+    const model = req.body.model
+    const year = req.body.year
+    const items = req.body.items
+
+    const sqlVerifyCar = `SELECT * FROM cars WHERE id = '${id}'`
+    pool.query(sqlVerifyCar, (err, data) => {
+        if (err) {
+            console.log(err)
+        }
+
+        if (!data.length != 0) {
+            res.status(404).json({message: "car not found"})
+            return
+        }
+
+        if (parseInt(year) < 2015 || parseInt(year) > 2025) {
+            return res.status(400).json({ message: "year should be between 2015 and 2025" });
+        }
+
+        // verificando carro já existente
+        const sqlVerifyAlredyExistCar = `SELECT * FROM cars WHERE (brand = '${brand}') AND (model = '${model}') AND (year = '${year}')`
+        pool.query(sqlVerifyAlredyExistCar, (err, data) => {
+            if (err) {
+                console.log(err)
+            }
+
+            if(data.length !=0) {
+                res.status(409).json({ message: "there is already a car with this data" });
+                return
+            }
+
+            const validData = {}
+
+            if(brand && brand.trim()) validData.brand = brand
+            if(model && model.trim()) validData.model = model
+            if(year !== undefined && year !== null) validData.year = year
+
+            if (Object.keys(validData).length > 0) {
+                // fazendo uma query dinamica para aceitar somente dados que não são nulos e vazios
+                const fields = Object.keys(validData).map((field) => `${field} = ?`).join(', ')
+                const values = Object.values(validData)
+            
+                const sqlUpdateCar = `UPDATE cars SET ${fields} WHERE id = '${id}'`
+                pool.query(sqlUpdateCar, values, (err) => {
+                    if (err) {
+                        console.log(err)
+                    }                
+                })
+            }
+            
+
+            if (items && Array.isArray(items)) {
+                const uniqueItems = [...new Set(items.filter(item => item && item.trim()))];
+
+                if(uniqueItems.length > 0) {
+                    // remove os itens antigos
+                    const sqlDeleteItems = `DELETE FROM cars_items WHERE car_id = ?`;
+                    pool.query(sqlDeleteItems, [id], (err) => {
+                        if(err) {
+                            console.log(err)
+                        }
+                    })
+
+                    const values = uniqueItems.map(item => [item.trim(), id]);
+                    console.log(values)
+
+                    const sqlInsertItems = `INSERT INTO cars_items (name, car_id) VALUES ?`
+                    pool.query(sqlInsertItems, [values], (err) => {
+                        if(err) {
+                            console.log(err)
+                        }
+                    })
+                }
+            }
+            res.status(204)
+            res.end()
+        })
+    })
+})
+
 app.listen(3000)
